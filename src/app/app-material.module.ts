e5841ab07c0f0fb2e1992d0d2bdea31e263808ac
mdanial <daniyal@ideabox.pk>
2020-04-14 00:48:04 +0500

    Boiler Plate

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,102 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule, MatPaginatorModule } from '@angular/material';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material';
+import { MatSortModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatBadgeModule } from '@angular/material/badge';
+
+@NgModule({
+    imports: [
+        MatAutocompleteModule,
+        MatSlideToggleModule,
+        MatStepperModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatProgressSpinnerModule,
+        MatPaginatorModule,
+        MatMenuModule,
+        MatSortModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatListModule,
+        MatIconModule,
+        MatDividerModule,
+        MatTabsModule,
+        MatButtonModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatTableModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatChipsModule,
+        MatGridListModule,
+        MatTooltipModule,
+        MatExpansionModule,
+        MatSnackBarModule,
+        MatProgressBarModule,
+        MatBadgeModule,
+    ],
+    exports: [
+        MatAutocompleteModule,
+        MatSlideToggleModule,
+        MatStepperModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatProgressSpinnerModule,
+        MatPaginatorModule,
+        MatMenuModule,
+        MatSortModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatListModule,
+        MatIconModule,
+        MatDividerModule,
+        MatTabsModule,
+        MatButtonModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatTableModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatChipsModule,
+        MatGridListModule,
+        MatTooltipModule,
+        MatExpansionModule,
+        MatSnackBarModule,
+        MatProgressBarModule,
+        MatBadgeModule,
+    ],
+
+}) export class MaterialModule { }
